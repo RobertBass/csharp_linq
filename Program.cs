9767ed0f57dda95cb63ac7211bdae283db840a1b
robertbass <robertbass@hotmail.com>
2025-01-05 00:33:34 -0500

    FIRST USE OF LINQ. ADDING JSON FILE FOR USE OF LINQ

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program {
+    static void Main(String[] args) {
+        var frutas = new string[]{
+            "Sandia",
+            "Fresa",
+            "Mango",
+            "Mango de Azucar",
+            "Mango de Tomy"
+        };
+
+        var mangoList = frutas.Where(p=> p.StartsWith("Mango")).ToList();
+
+        mangoList.ForEach(p=> Console.WriteLine(p));
+    }
+}
+
+
